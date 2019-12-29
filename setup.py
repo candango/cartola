58e28741c5747ad25b70ef71b14403801ceed7d4
Flavio Garcia <piraz@candango.org>
2019-12-29 09:47:21 -0500

    Introduced project's basic structure.

---
+++ setup.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python
+#
+# Copyright 2019-2020 Flavio Garcia
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import cartola
+from setuptools import setup
+from codecs import open
+try:
+    # for pip >= 10
+    from pip._internal.req import parse_requirements
+except ImportError:
+    # for pip <= 9.0.3
+    from pip.req import parse_requirements
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+
+# Solution from http://bit.ly/29Yl8VN
+def resolve_requires(requirements_file):
+    requirements = parse_requirements("./%s" % requirements_file,
+            session=False)
+    return [str(ir.req) for ir in requirements]
+
+
+setup(
+    name="cartola",
+    version=cartola.get_version(),
+    license=cartola.__licence__,
+    description=("Candango's Shared Util Functions."),
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/candango/automatoes",
+    author=cartola.get_author(),
+    author_email=cartola.get_author_email(),
+    classifiers=[
+        "Development Status :: 2 - Pre-Alpha",
+        "License :: OSI Approved :: Apache Software License",
+        "Environment :: Console",
+        "Environment :: Web Environment",
+        "Intended Audience :: Developers",
+        "Intended Audience :: System Administrators",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3 :: Only",
+    ],
+    packages=["cartola"],
+    install_requires=resolve_requires("requirements/basic.txt")
+)
