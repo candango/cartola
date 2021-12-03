a504e7416cf0873306bc83ae0a84c69c89c90b32
Flavio Garcia <piraz@candango.org>
2021-12-02 21:02:50 -0500

    Refactored paginator.
    Removed most of the logic from the constructor and spread around
    some properties as we don't need to keep some states inside the
    class.
    
    Most of properties and parameters with changed names kept the
    previous nomenclature but set as depreciated.
    
    Fixes: #47

--- tests/pagination_test.py
+++ tests/pagination_test.py
@@ -19,15 +19,105 @@ Pagination tests
 """
 
 from cartola import pagination
+import logging
+import warnings
 import unittest
+
+
+class WarningsHandler(logging.Handler):
+    def handle(self, record):
+        if record.levelno == logging.WARN:
+            warnings.warn(record.getMessage())
+        return record
 
 
 class PaginatorTestCase(unittest.TestCase):
 
+    def setUp(self):
+        self.logger = logging.getLogger()
+        self.handler = WarningsHandler()
+        #self.logger.addHandler(WarningsHandler())
+        warnings.simplefilter("always")
+
+    def tearDown(self):
+        self.logger.removeHandler(self.handler)
+
+    def test_paginator_depreciation(self):
+        """ Test paginator depreciation methods and parameters """
+        with warnings.catch_warnings(record=True) as w:
+            paginator = pagination.Paginator(9, current_page=1,
+                                             rows_per_page=10)
+            
+            warning = w.pop()
+            self.assertEqual("The parameter 'rows_per_page' is depreciated, "
+                             "use 'per_page' instead.",
+                             warning.message.args[0])
+            warning = w.pop()
+            self.assertEqual("The parameter 'current_page' is depreciated, "
+                             "use 'page' instead.", warning.message.args[0])
+            self.assertEqual(1, paginator.page_count)
+            warning = w.pop()
+            self.assertEqual("The property 'page_count' is depreciated, use "
+                             "'pages' instead.", warning.message.args[0])
+            self.assertEqual(1, paginator.pages)
+            self.assertEqual(9, paginator.row_count)
+            warning = w.pop()
+            self.assertEqual("The property 'row_count' is depreciated, use "
+                             "'count' instead.", warning.message.args[0])
+            self.assertEqual(1, paginator.current_first_row)
+            warning = w.pop()
+            self.assertEqual("The property 'current_first_row' is depreciated,"
+                             " use 'first_row' instead.",
+                             warning.message.args[0])
+            self.assertEqual(9, paginator.current_last_row)
+            warning = w.pop()
+            self.assertEqual("The property 'current_last_row' is depreciated,"
+                             " use 'last_row' instead.",
+                             warning.message.args[0])
+
     def test_paginator_one_page(self):
         """ Test paginator with one page """
-        paginator = pagination.Paginator(9, 1)
-        self.assertEqual(1, paginator.page_count)
-        self.assertEqual(9, paginator.row_count)
-        self.assertEqual(1, paginator.current_first_row)
-        self.assertEqual(9, paginator.current_last_row)
+        paginator = pagination.Paginator(7, page=2)
+        self.assertEqual(1, paginator.pages)
+        self.assertEqual(7, paginator.count)
+        self.assertEqual(1, paginator.first_row)
+        self.assertEqual(7, paginator.last_row)
+
+    def test_paginator_in_the_middle(self):
+        """ Test paginator with one page """
+        paginator = pagination.Paginator(64, page=5, per_page=6)
+        self.assertEqual(5, paginator.page)
+        self.assertEqual(11, paginator.pages)
+        self.assertEqual(64, paginator.count)
+        self.assertEqual(4, paginator.rows_in_last_page)
+        self.assertEqual(25, paginator.first_row)
+        self.assertEqual(30, paginator.last_row)
+
+    def test_paginator_rows_last_page_less_than_per_page(self):
+        """ Test paginator with one page """
+        paginator = pagination.Paginator(93, page=15, per_page=7)
+        self.assertEqual(14, paginator.page)
+        self.assertEqual(14, paginator.pages)
+        self.assertEqual(93, paginator.count)
+        self.assertEqual(2, paginator.rows_in_last_page)
+        self.assertEqual(92, paginator.first_row)
+        self.assertEqual(93, paginator.last_row)
+
+    def test_paginator_rows_last_page_equal_per_page(self):
+        """ Test paginator with one page """
+        paginator = pagination.Paginator(30, page=3)
+        self.assertEqual(3, paginator.pages)
+        self.assertEqual(3, paginator.page)
+        self.assertEqual(30, paginator.count)
+        self.assertEqual(21, paginator.first_row)
+        self.assertEqual(30, paginator.last_row)
+
+    def test_offset(self):
+        """ Test the offset method to be used directly with a database query
+        implementation """
+        paginator = pagination.Paginator(9)
+
+    def test_offsets(self):
+        """ Test the offset generator too loop through all offsets existent
+        from page 1 to last page """
+        paginator = pagination.Paginator(9)
