d77c33d268d097d80607208e77d12f7f08bec258
Flavio Garcia <piraz@candango.org>
2022-11-06 20:21:45 -0500

    Bump actions/checkout to v3

--- setup.py
+++ setup.py
@@ -51,7 +51,7 @@ setup(
     name="cartola",
     version=cartola.get_version(),
     license=cartola.__licence__,
-    description="Candango's Shared Util Functions.",
+    description="Candango's Shared Functions.",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/candango/cartola",
@@ -68,6 +68,8 @@ setup(
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
         "Programming Language :: Python :: 3 :: Only",
         "Topic :: Software Development :: Libraries"
     ],
