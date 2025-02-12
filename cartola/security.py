66e2a1863c5d4439ccb4ca93ade67175d2b63e1f
Flavio Garcia <piraz@candango.org>
2025-02-11 22:27:18 -0500

    fix(security): Remove all KeyManager from security
    Fixes: #74

--- cartola/security.py
+++ cartola/security.py
@@ -1,6 +1,4 @@
-# -*- coding: UTF-8 -*-
-#
-# Copyright 2015-2023 Flavio Garcia
+# Copyright 2015-2025 Flavio Garcia
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -14,10 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import os
-if os.name == 'posix':
-    import crypt
-from hmac import compare_digest as compare_hash
 import logging
 import string
 
@@ -44,94 +38,3 @@ def random_string(length=5, upper_chars=True, punctuation=False):
         chars += string.punctuation
     import secrets
     return ''.join(secrets.choice(chars) for _ in range(length))
-
-
-# Key manager functionality is available only for posix.
-# TODO: Figure out how to get this working on nt(windows).
-if os.name == 'posix':
-    class KeyManager(object):
-        # following: https://crackstation.net/hashing-security.htm
-        METHOD_BLOWFISH = crypt.METHOD_BLOWFISH
-        METHOD_MD5 = crypt.METHOD_MD5
-        METHOD_CRYPT = crypt.METHOD_CRYPT
-        METHOD_SHA512 = crypt.METHOD_SHA512
-        METHOD_SHA256 = crypt.METHOD_SHA256
-
-        @staticmethod
-        def get_manager(method):
-            """ Return a key manager by its key method.
-
-            :param method:
-            :return: A key manger by its key method
-            :rtype: KeyManager
-            """
-            managers = {
-                KeyManager.METHOD_CRYPT: CryptKeyManager,
-                KeyManager.METHOD_MD5: Md5Manager,
-                KeyManager.METHOD_SHA512: Sha512KeyManager,
-                KeyManager.METHOD_SHA256: Sha256KeyManager,
-                KeyManager.METHOD_BLOWFISH: BlowfishKeyManager,
-            }
-            manager = managers.get(method, NotImplemented)
-            return manager()
-
-        def generate(self, secret, **kwargs):
-            """ Generate a hash using crypt the method will be chosen from the
-            salt implemented or provided. If salt isn't provided a salt will be
-            generated during the processing, it is necessary to extract the
-            salt from the returned hash in order to validate it again.
-
-            :param str secret:
-            :param dict kwargs:
-            :key salt:
-            :key pepper:
-            :return:
-            :rtype: str
-            """
-            salt = kwargs.get("salt", self.salt(**kwargs))
-            pepper = kwargs.get("pepper")
-            if pepper:
-                secret = "%s%s" % (secret, pepper)
-            return crypt.crypt(secret, salt)
-
-        @staticmethod
-        def salt_from_hash(_hash):
-            return "$".join(_hash.split("$")[:-1])
-
-        def salt(self, **kwargs):
-            raise NotImplementedError
-
-        def _salt_it(self, method, **kwargs):
-            rounds = kwargs.get("rounds")
-            salt = crypt.mksalt(method=method, rounds=rounds)
-            return salt
-
-        def validate(self, secret, _hash, **kwargs):
-            salt = kwargs.get("salt", self.salt_from_hash(_hash))
-            kwargs['salt'] = salt
-            return compare_hash(self.generate(secret, **kwargs), _hash)
-
-    class BlowfishKeyManager(KeyManager):
-
-        def salt(self, **kwargs):
-            return self._salt_it(crypt.METHOD_BLOWFISH, **kwargs)
-
-    class CryptKeyManager(KeyManager):
-
-        def salt(self, **kwargs):
-            return self._salt_it(crypt.METHOD_CRYPT, **kwargs)
-
-    class Md5Manager(KeyManager):
-
-        def salt(self, **kwargs):
-            return self._salt_it(crypt.METHOD_MD5, **kwargs)
-
-    class Sha512KeyManager(KeyManager):
-
-        def salt(self, **kwargs):
-            return self._salt_it(crypt.METHOD_SHA512, **kwargs)
-
-    class Sha256KeyManager(KeyManager):
-
-        def salt(self, **kwargs):
-            return self._salt_it(crypt.METHOD_SHA256, **kwargs)
--- tests/security_test.py
+++ tests/security_test.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 #
-# Copyright 2015-2023 Flavio Garcia
+# Copyright 2015-2025 Flavio Garcia
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -18,9 +18,8 @@
 Security tests
 """
 
-from cartola.security import random_string, KeyManager
+from cartola.security import random_string
 import unittest
-import os
 
 
 class RandomStringTestCase(unittest.TestCase):
@@ -34,55 +33,3 @@ class RandomStringTestCase(unittest.TestCase):
         self.assertEqual(len(random_string(10, punctuation=True)), 10)
         self.assertEqual(len(random_string(10, upper_chars=False,
                                            punctuation=True)), 10)
-
-
-# Key manager functionality is available only for posix.
-if os.name == 'posix':
-    class KeyManagerTestCase(unittest.TestCase):
-
-        def setUp(self):
-            self.password = "A_secret_text"
-            self.bad_password = "A_bad_secret_text"
-            self.pepper = "We_have_salt_and_need_pepper"
-
-        def run_sha_with_method_no_pepper(self, method):
-            key_manager = KeyManager.get_manager(method)
-            hash_result = key_manager.generate(self.password)
-            salt = key_manager.salt_from_hash(hash_result)
-            self.assertTrue(hash_result.startswith(salt))
-            self.assertTrue(key_manager.validate(self.password, hash_result))
-            self.assertFalse(key_manager.validate(self.bad_password,
-                                                  hash_result))
-
-        def run_sha_with_method_peppered(self, method):
-            key_manager = KeyManager.get_manager(method)
-            hash_result = key_manager.generate(self.password,
-                                               pepper=self.pepper)
-            salt = key_manager.salt_from_hash(hash_result)
-            self.assertTrue(hash_result.startswith(salt))
-            self.assertTrue(key_manager.validate(self.password, hash_result,
-                                                 pepper=self.pepper))
-            self.assertFalse(key_manager.validate(self.password, hash_result))
-            self.assertFalse(key_manager.validate(self.bad_password,
-                                                  hash_result,
-                                                  pepper=self.pepper))
-
-        def test_sha512_without_pepper(self):
-            """ Manager generation and verification on SHA512 without pepper"""
-            self.run_sha_with_method_no_pepper(KeyManager.METHOD_SHA512)
-
-        def test_sha512_with_pepper(self):
-            """ Manager generation and verification on SHA512 and pepper"""
-            self.run_sha_with_method_peppered(KeyManager.METHOD_SHA512)
-
-        def test_sha256_without_pepper(self):
-            """ Manager generation and verification on SHA256 without pepper"""
-            self.run_sha_with_method_no_pepper(KeyManager.METHOD_SHA256)
-
-        def test_sha256_with_pepper(self):
-            """ Manager generation and verification on SHA256 and pepper"""
-            self.run_sha_with_method_peppered(KeyManager.METHOD_SHA256)
-
-        def test_blowfish_with_pepper(self):
-            """ Manager generation and verification on SHA256 and pepper"""
-            self.run_sha_with_method_peppered(KeyManager.METHOD_MD5)
